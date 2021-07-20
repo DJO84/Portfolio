5149459bdad21c3122c997298fd235a976156bf0
Ubuntu <dave_olsen84@hotmail.com>
2021-07-20 02:07:29 +0000

    Updated structure and comments

--- Week2/foldermaker.sh
+++ Week2/foldermaker.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#Creating a folder
+#David Olsen
+#20/07/2021
+
 read -p "Type the name of the folder you would like to create: " folderName
 mkdir "$folderName"
 echo "You have chosen to create : $folderName"
--- Week2/setPassword.sh
+++ Week2/setPassword.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-read -p "Please type a folder name: " folderName #getting user to input folder name
-read -sp "Please enter password" PassName #getting password from user
+#Creating a password that is Hashed to a file
+#David Olsen
+#20/07/2021
+
+read -p "Please type a folder name: " folderName #Requesting user to input folder name
+read -sp "Please enter password" PassName #Requesting password from user
 mkdir "$folderName" 
-echo $PassName | sha256sum > "$folderName/Secret.txt" #Saving password to user specified folder
+echo $PassName | sha256sum > "$folderName/Secret.txt" #Saving hash password to user specified folder
 echo
 echo "Credentails saved securely"
 exit 0
