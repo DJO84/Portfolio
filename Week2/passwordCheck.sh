9f2ff5bc55cd957d36ed07ca3387deeb72233093
Ubuntu <dave_olsen84@hotmail.com>
2021-07-20 01:54:50 +0000

    Password check with comments and structure

---
+++ Week2/passwordCheck.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#Check that the password entered matches the saved HASH within a file
+
+read -sp "Please type your secret password: " PassName #Ask user for Password
+echo
+
+#Run User Entered Password through HASH and check file to return code
+echo "$PassName" | sha256sum --status -c Passwords/Secret.txt
+
+#Check return code and display appropriate message
+
+if [[ $? -eq 0 ]]; then
+    echo "Access Granted."
+    exit 0
+else 
+    echo "Access Denied."
+    exit 1
+fi
