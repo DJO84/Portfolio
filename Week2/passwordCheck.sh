50a0326c07c8ffc74dec05990d4e44e7f9fa3b3d
Ubuntu <dave_olsen84@hotmail.com>
2021-07-26 01:16:30 +0000

    Updated for megamenu

--- Week2/passwordCheck.sh
+++ Week2/passwordCheck.sh
@@ -3,17 +3,19 @@
 #David Olsen
 #20/07/2021
 
-read -sp "Please type your secret password: " PassName #Ask user for Password
+echo -e "\e[1;31m"
+read -sp "Please type your secret password:" PassName #Ask user for Password
+echo -e "\e[0m"
 echo
 
 #Run User Entered Password through HASH and check file to return code
-echo "$PassName" | sha256sum --status -c Passwords/Secret.txt
+echo "$PassName" | sha256sum --status -c /home/student/scripts/portfolio/Week2/Passwords/Secret.txt
 
 #Check return code and display appropriate message
 if [[ $? -eq 0 ]]; then
-    echo "Access Granted."
+    echo -e "\e[1;32m Access Granted. \e[0m"
     exit 0
 else 
-    echo "Access Denied."
+    echo -e "\e[1;31m Access Denied. \e[0m"
     exit 1
 fi
