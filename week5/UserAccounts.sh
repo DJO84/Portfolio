91b50306e3e4dd48ffc5ec5ee6f4817a63c6e276
Ubuntu <dave_olsen84@hotmail.com>
2021-08-04 06:16:03 +0000

    Regex used to filter

--- week5/UserAccounts.sh
+++ week5/UserAccounts.sh
@@ -2,9 +2,14 @@
 #Displaying passwd file edited and in a viewable table With the addon of filtering listed users based on their default shell location
 #David Olsen
 #02/08/2021
+#modified 04/08/2021
+
+
+#Regex to filter only bin/bash in field 7 and pass output to next awk command 
+awk -F: '$7~/^\/\w{3}\/\w[a-b]\w{2}$/' /etc/passwd | 
 
 #Use Awk to display the data in a table utilising different colours.
-awk 'BEGIN {
+awk 'BEGIN { 
 
     FS=":"
     print "___________________________________________________________________________________________________";
@@ -14,8 +19,8 @@ awk 'BEGIN {
 }
 
 {
-    if($7 == "/bin/bash") #New addition to file to filter output to only users with /bin/bash listed as their default shell in coloumn $7
-    printf("| \033[33m%-16s\033[0m | \033[35m%-6s\033[0m | \033[35m%-7s\033[0m | \033[35m%-35s\033[0m | \033[35m%-20s\033[0m |\n", $1, $3, $4, $6, $7);
+    
+    printf("| \033[33m%-16s\033[0m | \033[35m%-6s\033[0m | \033[35m%-7s\033[0m | \033[35m%-35s\033[0m | \033[35m%-20s\033[0m |\n", $1, $3, $4, $6, $7); #printing results and lining up spacings
 
 }
 
@@ -23,6 +28,6 @@ END {
 
     print("|__________________|________|_________|_____________________________________|______________________|"); #lining up pipes to end final table look
 
-}' /etc/passwd
+}'
 
 exit 0
