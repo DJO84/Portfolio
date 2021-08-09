36744fca8b5f3691f063ca278591cbca82a4b66b
Ubuntu <dave_olsen84@hotmail.com>
2021-08-09 05:04:06 +0000

    Updated commenting structure

--- week1/second.sh
+++ week1/second.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#Create a simple script that takes the input from a user when running the scipt
+#David Olsen
+#Modified 09/08/2021
+
 echo "Hi there!"
 echo "Its good to see you $1!"
 exit 0
